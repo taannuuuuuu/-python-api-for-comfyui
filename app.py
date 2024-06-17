ea715f02e58510115f5f42cf9376b241565830e9
taannuuuuuu <97411462+taannuuuuuu@users.noreply.github.com>
2024-06-17 16:45:55 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,44 @@
+import json
+import gradio as gr
+import requests
+import os
+import time
+
+URL = "http://127.0.0.1:8188/prompt"
+OUTPUT_DIR="/Users/tanuraman/ComfyUI/output"
+
+def get_latest_image(folder):
+    files=os.listdir(folder)
+    image_files=[f for f in files if f.lower().endswith(('.png','.jpeg','.jpg'))]
+    image_files.sort(key= lambda x: os.path.getmtime(os.path.join(folder,x)))
+    latest_image=os.path.join(folder, image_files[-1]) if image_files else None
+    return latest_image
+        
+
+def start_queue(prompt_workflow):
+    p={"prompt" : prompt_workflow}
+    data = json.dumps(p).encode('utf-8')
+    requests.post(URL, data=data)
+
+def generate_image(prompt_text,step_count):
+    with open("workflow_api-2.json","r") as file_json:
+        prompt =json.load(file_json)
+        prompt["6"]["inputs"]["text"] = f"digital artwork of a {prompt_text}"
+        prompt["3"]["inputs"]["steps"] = step_count
+
+    previous_image = get_latest_image(OUTPUT_DIR)
+
+    start_queue(prompt)
+    while True:
+        latest_image = get_latest_image(OUTPUT_DIR)
+        if latest_image != previous_image:
+            return latest_image
+        
+        time.sleep(1)
+
+
+
+demo=gr.Interface(fn=generate_image, inputs=["text","text"] , outputs=["image"])
+
+demo.launch(share=True)
+
